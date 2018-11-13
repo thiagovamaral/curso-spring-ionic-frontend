caa48c571dc9de91d87fe6b01390c58d6df2cfef
Thiago Amaral <thiago-amaral@msn.com>
2018-11-13 12:59:27 -0200

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -17,7 +17,7 @@ export class ProfilePage {
   cliente: ClienteDTO;
 
   constructor(
-    public navCtrl: NavController, 
+    public navCtrl: NavController,
     public navParams: NavParams,
     public storage: StorageService,
     public clienteService: ClienteService) {
@@ -31,15 +31,22 @@ export class ProfilePage {
           this.cliente = response;
           this.getImageIfExists();
         },
-        error => {});
+          error => {
+            if (error.status == 403) {
+              this.navCtrl.setRoot('HomePage');
+            }
+          });
+    }
+    else {
+      this.navCtrl.setRoot('HomePage');
     }
   }
 
   getImageIfExists() {
     this.clienteService.getImageFromBucket(this.cliente.id)
-    .subscribe(response => {
-      this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
-    },
-    error => {});
+      .subscribe(response => {
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`;
+      },
+        error => { });
   }
 }
