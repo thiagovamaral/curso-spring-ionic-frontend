969a5d6f254781e55d53402b3d8ef194646a61b6
Thiago Amaral <thiago-amaral@msn.com>
2018-11-13 07:56:41 -0200

    Obtendo os dados do formulário de login

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -4,21 +4,25 @@ import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    
+
      intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("Passou no interceptor");
         return next.handle(req)
         .catch((error, caught) => {
-             let errorObj = error;
+            let errorObj = error;
+            
             if (errorObj.error) {
                 errorObj = errorObj.error;
             }
+            
             if (!errorObj.status) {
                 errorObj = JSON.parse(errorObj);
             }
-             console.log("Erro detectado pelo interceptor:");
+            
+            console.log("Erro detectado pelo interceptor:");
             console.log(errorObj);
-             return Observable.throw(errorObj);
+            
+            return Observable.throw(errorObj);
         }) as any;
     }
 }
