5c5baddf92ea9f4d9a0724d1c79a04417ed639a4
Thiago Amaral <thiago-amaral@msn.com>
2018-11-27 11:20:41 -0200

    Testando o app no dispositivo

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://curso-java-thiago.herokuapp.com/",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/curso-java"
 }
